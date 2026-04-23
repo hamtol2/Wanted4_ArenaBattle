890b46b1f07fe1b7e0d7780e64b39c34050cefdd
ronniej <ronniej@naver.com>
2026-04-23 16:55:24 +0900

    스테이지 기믹 액터 컴포넌트 구성 완료
    - 바닥, 트리거, 문, 문 트리거 컴포넌트 추가
    - 새로운 맵 생성
    - 문 트리거에 부딪힐 때 태그 값 출력 (데모)

--- Source/ArenaBattle/Gimmick/ABStageGimmick.cpp
+++ Source/ArenaBattle/Gimmick/ABStageGimmick.cpp
@@ -38,6 +38,67 @@ AABStageGimmick::AABStageGimmick()
 		this,
 		&AABStageGimmick::OnStageTriggerBeginOverlap
 	);
+
+	// 문 컴포넌트.
+	static FName GateSockets[] =
+	{
+		TEXT("+XGate"),
+		TEXT("-XGate"),
+		TEXT("+YGate"),
+		TEXT("-YGate"),
+	};
+	// 문 메시 로드.
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> GateMeshRef(
+		TEXT("/Game/ArenaBattle/Environment/Props/SM_GATE.SM_GATE")
+	);
+
+	// 배열 순회하면서 문 메시 생성.
+	for (FName GateSocket : GateSockets)
+	{
+		// 컴포넌트 생성.
+		UStaticMeshComponent* Gate
+			= CreateDefaultSubobject<UStaticMeshComponent>(GateSocket);
+		// 생성된 컴포넌트에 메시 애셋 설정.
+		if (GateMeshRef.Succeeded())
+		{
+			Gate->SetStaticMesh(GateMeshRef.Object);
+		}
+		// 컴포넌트 계층 설정(소켓 포함 지정).
+		Gate->SetupAttachment(Stage, GateSocket);
+		// 위치 / 회전 조정.
+		Gate->SetRelativeLocationAndRotation(
+			FVector(0.0f, -80.0f, 0.0f),
+			FRotator(0.0f, -90.0f, 0.0f)
+		);
+		
+		// Map에 생성한 컴포넌트 추가.
+		Gates.Add(GateSocket, Gate);
+
+		// 문 통과를 확인하기 위한 박스 컴포넌트.
+		// 예: +XGate + Trigger = +XGateTrigger.
+		FName TriggerName = *GateSocket.ToString().Append("Trigger");
+		UBoxComponent* GateTrigger
+			= CreateDefaultSubobject<UBoxComponent>(TriggerName);
+		// 박스 컴포넌트의 충돌 영역 설정.
+		GateTrigger->SetBoxExtent(FVector(100.0f, 100.0f, 300.0f));
+		// 생성된 박스 컴포넌트의 계층 및 소켓 설정.
+		GateTrigger->SetupAttachment(Stage, GateSocket);
+		// 상대위치 조정.
+		GateTrigger->SetRelativeLocation(FVector(70.0f, 0.0f, 250.0f));
+		// 콜리전 프로필 설정.
+		GateTrigger->SetCollisionProfileName(CPROFILE_ABTRIGGER);
+		// 충돌 이벤트에 함수 등록 (델리게이트).
+		GateTrigger->OnComponentBeginOverlap.AddDynamic(
+			this,
+			&AABStageGimmick::OnGateTriggerBeginOverlap
+		);
+
+		// 트리거 구분을 위해 태그 추가.
+		GateTrigger->ComponentTags.Add(GateSocket);
+
+		// 생성된 트리거 컴포넌트를 배열에 추가.
+		GateTriggers.Add(GateTrigger);
+	}
 }
 
 void AABStageGimmick::OnStageTriggerBeginOverlap(
@@ -58,4 +119,10 @@ void AABStageGimmick::OnGateTriggerBeginOverlap(
 	bool bFromSweep, 
 	const FHitResult& SweepResult)
 {
+	UE_LOG(
+		LogTemp,
+		Log,
+		TEXT("Gate tag: %s"),
+		*OverlappedComponent->ComponentTags[0].ToString()
+	);
 }
