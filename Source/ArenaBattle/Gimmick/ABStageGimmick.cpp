605910b3997632b127db9b5499b80c9d64a6646c
ronniej <ronniej@naver.com>
2026-04-23 15:51:05 +0900

    무한맵 제작을 위한 액터 생성 및 기본 컴포넌트 구성(문 추가해야 함)

---
+++ Source/ArenaBattle/Gimmick/ABStageGimmick.cpp
@@ -0,0 +1,61 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Gimmick/ABStageGimmick.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
+#include "Physics/ABCollision.h"
+
+// Sets default values
+AABStageGimmick::AABStageGimmick()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	// 컴포넌트 생성.
+	Stage = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Stage"));
+	RootComponent = Stage;
+
+	// 메시 애셋 로드 및 설정.
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> StageMeshRef(
+		TEXT("/Game/ArenaBattle/Environment/Stages/SM_SQUARE.SM_SQUARE")
+	);
+	if (StageMeshRef.Succeeded())
+	{
+		Stage->SetStaticMesh(StageMeshRef.Object);
+	}
+
+	StageTrigger = CreateDefaultSubobject<UBoxComponent>(
+		TEXT("StageTrigger")
+	);
+	// 박스 컴포넌트 충돌 영역 크기 설정.
+	StageTrigger->SetBoxExtent(FVector(775.0f, 775.0f, 300.0f));
+	// 계층 설정.
+	StageTrigger->SetupAttachment(Stage);
+	StageTrigger->SetRelativeLocation(FVector(0.0f, 0.0f, 250.0f));
+	StageTrigger->SetCollisionProfileName(CPROFILE_ABTRIGGER);
+	StageTrigger->OnComponentBeginOverlap.AddDynamic(
+		this,
+		&AABStageGimmick::OnStageTriggerBeginOverlap
+	);
+}
+
+void AABStageGimmick::OnStageTriggerBeginOverlap(
+	UPrimitiveComponent* OverlappedComponent, 
+	AActor* OtherActor, 
+	UPrimitiveComponent* OtherComp, 
+	int32 OtherBodyIndex, 
+	bool bFromSweep, 
+	const FHitResult& SweepResult)
+{
+}
+
+void AABStageGimmick::OnGateTriggerBeginOverlap(
+	UPrimitiveComponent* OverlappedComponent, 
+	AActor* OtherActor, 
+	UPrimitiveComponent* OtherComp, 
+	int32 OtherBodyIndex, 
+	bool bFromSweep, 
+	const FHitResult& SweepResult)
+{
+}
---
+++ Source/ArenaBattle/Gimmick/ABStageGimmick.h
@@ -0,0 +1,59 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ABStageGimmick.generated.h"
+
+UCLASS()
+class ARENABATTLE_API AABStageGimmick : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AABStageGimmick();
+
+	// Stage Section.
+protected:
+	// 스테이지를 보여줄 메시 컴포넌트.
+	UPROPERTY(VisibleAnywhere, Category = Stage)
+	TObjectPtr<class UStaticMeshComponent> Stage;
+
+	// 박스 컴포넌트 (오버랩 이벤트 처리를 위한 컴포넌트).
+	UPROPERTY(VisibleAnywhere, Category = Stage)
+	TObjectPtr<class UBoxComponent> StageTrigger;
+
+	// StageTrigger에 등록할 오버랩 이벤트 처리 함수.
+	UFUNCTION()
+	void OnStageTriggerBeginOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+
+	// Gate Section(4개 문).
+protected:
+	// 4개의 문을 Map으로 관리.
+	UPROPERTY(VisibleAnywhere, Category = Gate)
+	TMap<FName, TObjectPtr<class UStaticMeshComponent>> Gates;
+
+	// 각 문을 담당하는 콜리전을 배열로 관리.
+	UPROPERTY(VisibleAnywhere, Category = Gate)
+	TArray<TObjectPtr<class UBoxComponent>> GateTriggers;
+
+	// 각 문의 이벤트를 처리할 함수.
+	UFUNCTION()
+	void OnGateTriggerBeginOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+};
