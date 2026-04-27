facd8a51f779773c9d5964b8aa089aa9d3ee3e4f
ronniej <ronniej@naver.com>
2026-04-27 15:54:44 +0900

    지연 생성 로직 적용 / 무기 아이템에 부가 스탯 데이터 추가 / config를 활용해서 랜덤 메시 설정하는 기능 구현
    - 10강 실습 완료
    - SpawnActorDeferred 함수를 생성할 때 활용 (필요한 곳 모두)
    - 무기 아이템에 부가 스탯 데이터를 추가하고, 무기 아이템 습득 시 적용되도록 로직 업데이트
    - DefaultArenaBattle.ini config를 활용해서 애셋 경로 기반 비동기 랜덤 메시 로딩 기능 추가

--- Source/ArenaBattle/Gimmick/ABStageGimmick.cpp
+++ Source/ArenaBattle/Gimmick/ABStageGimmick.cpp
@@ -229,16 +229,25 @@ void AABStageGimmick::OnGateTriggerBeginOverlap(
 		return;
 	}
 
-	// 충돌이 안됐으면(비어 있으면) 스테이지 액터 생성.
-	AABStageGimmick* NewGimmick = GetWorld()->SpawnActor<AABStageGimmick>(
-		NewLocation,
-		FRotator::ZeroRotator
+	// 생성 트랜스폼.
+	const FTransform SpawnTransform(
+		NewLocation
 	);
+
+	// 충돌이 안됐으면(비어 있으면) 스테이지 액터 생성.
+	AABStageGimmick* NewGimmick
+		= GetWorld()->SpawnActorDeferred<AABStageGimmick>(
+			AABStageGimmick::StaticClass(),
+			SpawnTransform
+		);
 
 	if (NewGimmick)
 	{
 		// 새로 생성된 스테이지의 레벨 +1.
 		NewGimmick->SetStageNum(CurrentStageNum + 1);
+
+		// 생성 처리 완료.
+		NewGimmick->FinishSpawning(SpawnTransform);
 	}
 }
 
@@ -435,17 +444,21 @@ void AABStageGimmick::SpawnRewardBoxes()
 		FVector WorldSpawnLocation
 			= GetActorLocation() + RewardBoxLocation.Value
 			+ FVector(0.0f, 0.0f, 30.0f);
-
-		// 상자 생성.
-		AActor* ItemActor = GetWorld()->SpawnActor(
-			RewardBoxClass,
-			&WorldSpawnLocation,
-			&FRotator::ZeroRotator
+		const FTransform SpawnTransform(
+			WorldSpawnLocation
 		);
 
-		// 아이템 상자로 형변환.
+		// 상자 생성.
+		//AActor* ItemActor
 		AABItemBox* RewardBoxActor
-			= Cast<AABItemBox>(ItemActor);
+			= GetWorld()->SpawnActorDeferred<AABItemBox>(
+				RewardBoxClass,
+				SpawnTransform
+			);
+
+		// 아이템 상자로 형변환.
+		//AABItemBox* RewardBoxActor
+		//	= Cast<AABItemBox>(ItemActor);
 		if (RewardBoxActor)
 		{
 			// 상자에 태그 추가.
@@ -460,6 +473,9 @@ void AABStageGimmick::SpawnRewardBoxes()
 
 			// 생성된 박스를 배열에 추가.
 			RewardBoxes.Add(RewardBoxActor);
+
+			// 생성 완료 처리.
+			RewardBoxActor->FinishSpawning(SpawnTransform);
 		}
 	}
 }
