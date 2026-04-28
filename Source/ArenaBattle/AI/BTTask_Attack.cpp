3177ddba8282ecbfb91d433e75acea3dd1bd895c
ronniej <ronniej@naver.com>
2026-04-28 11:59:38 +0900

    공격 범위 감지 데코레이터 구현
    - 비헤이비어 트리 업데이트
    - AttackInRange 데코레이터 Value 반환 함수 구현

---
+++ Source/ArenaBattle/AI/BTTask_Attack.cpp
@@ -0,0 +1,10 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/BTTask_Attack.h"
+
+UBTTask_Attack::UBTTask_Attack()
+{
+	// 노드 이름 설정.
+	NodeName = TEXT("Attack");
+}
