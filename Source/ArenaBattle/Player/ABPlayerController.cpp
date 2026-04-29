a6319e40e817ee5dfc476725f4c1eceffed2ae35
ronniej <ronniej@naver.com>
2026-04-29 09:53:57 +0900

    HUD 위젯 생성 및 화면에 추가 / 스탯 위젯 구성 중(위젯 블루프린트)
    - ABHUDWidget C++ 클래스 및 위젯 블루프린트 생성
    - 플레이어 컨트롤러에서 위젯 생성하고 화면에 추가 (AddToViewport)
    - 스탯 위젯 블루프린트 구성중

--- Source/ArenaBattle/Player/ABPlayerController.cpp
+++ Source/ArenaBattle/Player/ABPlayerController.cpp
@@ -2,6 +2,19 @@
 
 
 #include "Player/ABPlayerController.h"
+#include "UI/ABHUDWidget.h"
+
+AABPlayerController::AABPlayerController()
+{
+	// 프로젝트에서 클래스 정보 가져오기.
+	static ConstructorHelpers::FClassFinder<UABHUDWidget> ABHUDWidgetRef(
+		TEXT("/Game/ArenaBattle/UI/WBP_ABHUD.WBP_ABHUD_C")
+	);
+	if (ABHUDWidgetRef.Succeeded())
+	{
+		ABHUDWidgetClass = ABHUDWidgetRef.Class;
+	}
+}
 
 void AABPlayerController::BeginPlay()
 {
@@ -11,4 +24,14 @@ void AABPlayerController::BeginPlay()
 	// 게임 시작되면 뷰포트로 바로 입력 되도록.
 	FInputModeGameOnly GameOnlyInputMode;
 	SetInputMode(GameOnlyInputMode);
+
+	// 위젯 생성.
+	ABHUDWidget = CreateWidget<UABHUDWidget>(this, ABHUDWidgetClass);
+
+	// 위젯 객체가 잘 생성됐으면, 화면에 추가.
+	if (ABHUDWidget)
+	{
+		// 화면에 추가해 UI가 보일 수 있도록 설정.
+		ABHUDWidget->AddToViewport();
+	}
 }
--- Source/ArenaBattle/Player/ABPlayerController.h
+++ Source/ArenaBattle/Player/ABPlayerController.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -13,7 +13,20 @@ UCLASS()
 class ARENABATTLE_API AABPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+	AABPlayerController();
 	
 protected:
 	virtual void BeginPlay() override;
+
+	// HUD.
+protected:
+	// 클래스 정보. 클래스 정보를 바탕으로 실제 HUD 객체 생성.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = HUD)
+	TSubclassOf<class UABHUDWidget> ABHUDWidgetClass;
+
+	// 실제 생성된 HUD 객체.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = HUD)
+	TObjectPtr<class UABHUDWidget> ABHUDWidget;
 };
