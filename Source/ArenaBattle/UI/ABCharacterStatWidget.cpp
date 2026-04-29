6904cb431a367a0688189e1c7c5d69f8eebd5f0a
ronniej <ronniej@naver.com>
2026-04-29 15:58:11 +0900

    StatWidget에 텍스트 블록 업데이트하는 로직 완성
    - 리플렉션을 활용해 텍스트 블록을 키값으로 저장하고 데이터 설정하는 로직 구현
    - 13강 완료

--- Source/ArenaBattle/UI/ABCharacterStatWidget.cpp
+++ Source/ArenaBattle/UI/ABCharacterStatWidget.cpp
@@ -2,9 +2,99 @@
 
 
 #include "UI/ABCharacterStatWidget.h"
+#include "Components/TextBlock.h"
+
+void UABCharacterStatWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	// 맵 구성을 위해 리플렉션 활용.
+	// FABCharacterStat 구조체 정보에서 변수 이름을 활용.
+	for (TFieldIterator<FNumericProperty> PropIt(
+		FABCharacterStat::StaticStruct());
+		PropIt != nullptr;
+		++PropIt)
+	{
+		// 속성의 이름 값.
+		const FName PropKey(PropIt->GetName());
+
+		// 속성의 이름 값을 활용해 텍스트 블록의 이름 값 만들기.
+		// Base 스탯에 대한 이름 값.
+		const FName TextBaseControlName
+			= *FString::Printf(
+				TEXT("Txt%sBase"), *PropIt->GetName()
+			);
+
+		// Modifier 스탯에 대한 이름 값.
+		const FName TextModifierControlName
+			= *FString::Printf(
+				TEXT("Txt%sModifier"), *PropIt->GetName()
+			);
+
+		// 위젯 블루프린트에서 위젯 컨트롤 읽어오기.
+		UTextBlock* BaseTextBlock
+			= Cast<UTextBlock>(GetWidgetFromName(TextBaseControlName));
+		if (BaseTextBlock)
+		{
+			// 맵에 추가.
+			BaseLookup.Add(PropKey, BaseTextBlock);
+		}
+
+		// 위젯 블루프린트에서 위젯 컨트롤 읽어오기.
+		UTextBlock* ModifierTextBlock
+			= Cast<UTextBlock>(GetWidgetFromName(TextModifierControlName));
+		if (ModifierTextBlock)
+		{
+			// 맵에 추가.
+			ModifierLookup.Add(PropKey, ModifierTextBlock);
+		}
+	}
+}
 
 void UABCharacterStatWidget::UpdateStat(
 	const FABCharacterStat& BaseStat, 
 	const FABCharacterStat& ModifierStat)
 {
+	for (TFieldIterator<FNumericProperty> PropIt(
+		FABCharacterStat::StaticStruct());
+		PropIt != nullptr;
+		++PropIt)
+	{
+		// 속성 키 값.
+		const FName PropKey(PropIt->GetName());
+
+		// 리플렉션을 활용해 변수의 값 읽어오기.
+		float BaseData = 0.0f;
+		PropIt->GetValue_InContainer(
+			reinterpret_cast<const void*>(&BaseStat), &BaseData
+		);
+
+		// 리플렉션을 활용해 변수의 값 읽어오기.
+		float ModifierData = 0.0f;
+		PropIt->GetValue_InContainer(
+			reinterpret_cast<const void*>(&ModifierStat), &ModifierData
+		);
+
+		// 맵에서 Base 텍스트 블록을 가져와 값 설정.
+		UTextBlock** BaseTextBlockPtr = BaseLookup.Find(PropKey);
+		if (BaseTextBlockPtr)
+		{
+			// Float 값을 FString으로 변환.
+			FString FloatValue = FString::SanitizeFloat(BaseData);
+
+			// FString을 FText로 변환해서 텍스트 블록에 설정.
+			(*BaseTextBlockPtr)->SetText(FText::FromString(FloatValue));
+		}
+
+		// 맵에서 Modifier 텍스트 블록을 가져와 값 설정.
+		UTextBlock** ModifierTextBlockPtr = ModifierLookup.Find(PropKey);
+		if (ModifierTextBlockPtr)
+		{
+			// Float 값을 FString으로 변환.
+			FString FloatValue = FString::SanitizeFloat(ModifierData);
+
+			// FString을 FText로 변환해서 텍스트 블록에 설정.
+			(*ModifierTextBlockPtr)->SetText(FText::FromString(FloatValue));
+		}
+	}
 }
--- Source/ArenaBattle/UI/ABCharacterStatWidget.h
+++ Source/ArenaBattle/UI/ABCharacterStatWidget.h
@@ -16,10 +16,21 @@ class ARENABATTLE_API UABCharacterStatWidget : public UUserWidget
 	GENERATED_BODY()
 	
 public:
+	virtual void NativeConstruct() override;
+
 	// BaseStat, ModifierStat 정보를 받아 UI를 업데이트하는 함수.
 	// Stat 컴포넌트에 이벤트 바인딩(델리게이트)해 호출되도록 설정.
 	void UpdateStat(
 		const FABCharacterStat& BaseStat,
 		const FABCharacterStat& ModifierStat
 	);
+
+private:
+	// 스탯 위젯이 가진 텍스트 블록을 저장하는 맵(BaseStat).
+	UPROPERTY()
+	TMap<FName, class UTextBlock*> BaseLookup;
+
+	// 스탯 위젯이 가진 텍스트 블록을 저장하는 맵(ModifierStat).
+	UPROPERTY()
+	TMap<FName, class UTextBlock*> ModifierLookup;
 };
